57c3782ea24c968ab65e706ee5c2fd1f93ac1c36
İsa Zobu <isaazobu@gmail.com>
2022-07-02 02:59:41 +0300

    add questionservice

--- questions/services/questions.service.go
+++ questions/services/questions.service.go
@@ -1,1 +1,42 @@
-package service
+package questionservice
+
+import (
+	models "github.com/isazobu/dailyQuestionsAPI/questions/models"
+	questionrepo "github.com/isazobu/dailyQuestionsAPI/questions/repository/mongodb"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type QuestionService interface {
+	AddQuestion(question models.Question) (models.Question, error)
+	GetQuestionsByCategory(category string) ([]models.Question, error)
+	GetQuestionById(id string) (models.Question, error)
+	UpdateQuestion(question models.Question) (models.Question, error)
+	DeleteQuestion(id string) error
+}
+type questionService struct {
+	Repo questionrepo.Repo
+}
+
+func NewQuestionService(qRepo questionrepo.Repo) QuestionService {
+	return &questionService{Repo: qRepo}
+}
+
+func (q questionService) AddQuestion(question models.Question) (*mongo.InsertOneResult, error) {
+	return q.Repo.AddQuestion(question)
+}
+
+func (q questionService) GetQuestionsByCategory(category string) ([]models.Question, error) {
+	return q.Repo.GetQuestionsByCategory(category)
+}
+
+func (q questionService) GetQuestionById(id string) (models.Question, error) {
+	return q.Repo.GetQuestionById(id)
+}
+
+func (q questionService) UpdateQuestion(question models.Question) (*mongo.UpdateResult, error) {
+	return q.Repo.UpdateQuestion(question)
+}
+
+func (q questionService) DeleteQuestion(id string) error {
+	return q.Repo.DeleteQuestion(id)
+}
